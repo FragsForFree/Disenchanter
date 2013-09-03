d589eee4d458357be8ff815c58627345653548bb
Xavier Thompson <xthompson13@gmail.com>
2013-09-04 00:00:27 +0800

    Added a config option to toggle between random enchanting and multi-mode enchanting. See http://dev.bukkit.org/bukkit-plugins/disenchanter/ for further information regarding configuration. Suggestion by Kino876. Thanks :)

--- src/com/gmail/xthompson13/Disenchanter/Disenchanter.java
+++ src/com/gmail/xthompson13/Disenchanter/Disenchanter.java
@@ -20,20 +20,34 @@ import org.bukkit.inventory.meta.ItemMeta;
 public class Disenchanter extends JavaPlugin{
 
 	public void onEnable(){
-	
+		getConfig().options().copyDefaults(true);
+		getConfig().addDefault("multi-mode", "false");
+		saveConfig();
 	}
 	
 	public void onDisable(){
-		
+		saveConfig();
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		
-		
+	
 	// Filter for console sending the command
 	if(sender instanceof Player){
 		// Listen for the command
-		if(cmd.getName().equalsIgnoreCase("disenchant")){
+		if(cmd.getName().equalsIgnoreCase("disenchanter")){
+			if(sender.hasPermission("disenchanter.admin")){
+				if(args[0].equalsIgnoreCase("set")){
+					if(args.length == 3){
+						getConfig().set(args[1], args[2]);
+						sender.sendMessage(ChatColor.GREEN+"[Disenchanter] Config option: "+ChatColor.GRAY+args[1]+ChatColor.GREEN+" set to: "+ChatColor.GRAY+args[2]);
+						saveConfig();
+						return true;
+					}
+				}
+			}
+		}
+		if(cmd.getName().equalsIgnoreCase("disenchant") && args.length == 0){
 		
 		// Initialise the player
 		final Player p = (Player) sender;
@@ -50,7 +64,6 @@ public class Disenchanter extends JavaPlugin{
 					
 					// Check to see if the player has the book in his/her inventory
 					if(p.getInventory().contains(340) == true){
-						
 						// Check to see if the player is carrying an emerald
 						if(p.getInventory().contains(388) == true){
 							
@@ -89,21 +102,33 @@ public class Disenchanter extends JavaPlugin{
 	
 								// Begin bukkit runnable for the delayed task.
 								if(checkval >= 1){
-									p.sendMessage(ChatColor.GREEN+"[Disenchanter] The ritual will begin...");
-									p.setItemInHand(new ItemStack(Material.AIR));
-									
-									// Confiscate the soul emerald...somehow
-									// We'll try creating a new item stack with the same meta data, but with a quantity of one.
 									
 									if (checkval >= 1){
+										
+										if(!p.getInventory().containsAtLeast(new ItemStack(Material.BOOK), sacrificeToRemove(item))){
+											p.sendMessage(ChatColor.RED+"[Disenchanter] You don't have enough books.");
+											return true;
+										}
 										// Populate information to remove the emerald. For some reason, a new item stack is needed.
 										ItemMeta emerMeta = emeraldCheck.getItemMeta();
 										ItemStack removeEmerald = new ItemStack(Material.EMERALD);
 										removeEmerald.setItemMeta(emerMeta);
-										removeEmerald.setAmount(1);
+										if(!p.getInventory().containsAtLeast(removeEmerald, sacrificeToRemove(item))){
+											p.sendMessage(ChatColor.RED+"[Disenchanter] You don't have enough Soul Emeralds.");
+											return true;
+										}
+										p.sendMessage(ChatColor.GREEN+"[Disenchanter] The ritual will begin...");
+										p.setItemInHand(new ItemStack(Material.AIR));
+
+										removeEmerald.setAmount(sacrificeToRemove(item));
 										p.getInventory().removeItem(removeEmerald);
-										// Remove the existing blank book from the player's inventory
-										p.getInventory().removeItem(new ItemStack(Material.BOOK, 1));			
+										
+										//book removal
+										int bookRemoveCount = 1;
+										while (bookRemoveCount <= sacrificeToRemove(item)){									
+											p.getInventory().removeItem(new ItemStack(Material.BOOK, 1));
+											bookRemoveCount++;
+										}
 									} 
 									
 								}
@@ -113,43 +138,7 @@ public class Disenchanter extends JavaPlugin{
 										// Reject the process if the item has no enchantment. This may be the best way to prevent unwanted vaporisings.
 										
 											if(checkval >= 1){
-					
-													// Initialise the book to be given to the player
-													ItemStack book = new ItemStack(Material.ENCHANTED_BOOK);
-							
-																			
-													
-													// Retrieve a set of the enchantments and values, and convert it to a split string
-													Set<Enchantment> keySet = item.getEnchantments().keySet();
-													String enchString[] = keySet.toString().split(", E");
-													// find out how many enchantments were on the item
-													int numberOfEntries = keySet.size();
-													// this is a conversion for the index of split strings - it starts counting at zero
-													int i = numberOfEntries - 1;
-													int enchID = -1;
-													
-													// Logic to randomise which enchantment is passed on, should there be more than one.
-													if (numberOfEntries > 1){
-														Random rand = new Random();
-														i = rand.nextInt(numberOfEntries);
-														enchID = Integer.parseInt(enchString[i].replaceAll("[\\D]", ""));
-													} else{
-														enchID = Integer.parseInt(enchString[i].replaceAll("[\\D]",""));
-													}
-						
-													//Create a new Enchantment object to load with info, namely the ID found above.
-													Enchantment thisgoesonthebook = new EnchantmentWrapper(enchID);
-													//Pull the enchantment level from the original item using the enchantment above
-													int enchantLVL = item.getEnchantmentLevel(thisgoesonthebook);
-													// Clone the existing, empty meta from a blank enchanted book
-													EnchantmentStorageMeta meta = (EnchantmentStorageMeta)book.getItemMeta();
-													// Populate the meta data with the information gained above.
-													meta.addStoredEnchant(thisgoesonthebook, enchantLVL, true);
-													// Set the meta data onto our new book.
-													book.setItemMeta(meta);
-					
-													// Put the book with enchantmentmetadata loaded into the player's inventory, not hand.
-													p.getInventory().addItem(book);
+													unwrapEnchants(item, p);
 													p.sendMessage(ChatColor.GREEN+"[Disenchanter] The ritual is complete.");
 					
 											} else {
@@ -185,6 +174,7 @@ public class Disenchanter extends JavaPlugin{
 		
 		if(cmd.getName().equalsIgnoreCase("whatname")){
 			
+			if(sender.hasPermission("disenchant.whatname")){
 			Player pn = (Player) sender;
 			String itemName = "There is nothing in your hand.";
 			if(pn.getItemInHand().getAmount() != 0){
@@ -200,8 +190,97 @@ public class Disenchanter extends JavaPlugin{
 			
 			return true;
 		}
+		}
 			
 	} else { sender.sendMessage("You cannot run this command from console.");}
 	return false;
+	}
+	
+	public void unwrapEnchants(ItemStack item, Player p){
+		// prep an enchanted book to return
+		ItemStack newBook = new ItemStack(Material.ENCHANTED_BOOK);
+		
+		// prep some BookMeta yo
+		EnchantmentStorageMeta meta = (EnchantmentStorageMeta) newBook.getItemMeta();
+		
+		// Get a set of the enchantments listed for our item
+		Set<Enchantment> keySet = item.getEnchantments().keySet();
+		
+		// Get a string array that tells me crudely what was on the item
+		String enchString[] = keySet.toString().split(", E");
+		
+		// find out how many enchantments were on the item. Then we can "WHILE" it for all enchants or randomise for random enchants!
+		int numberOfEntries = keySet.size();
+		
+		// get a -1 variable to use in string searching and cutting
+		int i = numberOfEntries - 1;
+		
+		// prep a variable to receive the enchantment ID
+		int enchID = -1;
+		
+		// prep a variable to receive the enchantment level
+		int enchLevel = 1;
+		
+		//
+		
+		
+		if (numberOfEntries == 1){
+			enchID = Integer.parseInt(enchString[i].replaceAll("[\\D]",""));
+			Enchantment enchant = new EnchantmentWrapper(enchID);
+			enchLevel = item.getEnchantmentLevel(enchant); 
+			meta.addStoredEnchant(enchant, enchLevel, true);
+			newBook.setItemMeta(meta);
+			p.getInventory().addItem(newBook);
+		}
+		if (numberOfEntries > 1){
+			if(getConfig().getString("multi-mode").equalsIgnoreCase("false")){
+				Random rand = new Random();
+				i = rand.nextInt(numberOfEntries);
+				enchID = Integer.parseInt(enchString[i].replaceAll("[\\D]", ""));
+				Enchantment enchant = new EnchantmentWrapper(enchID);
+				enchLevel = item.getEnchantmentLevel(enchant); 
+				meta.addStoredEnchant(enchant, enchLevel, true);
+				newBook.setItemMeta(meta);
+				p.getInventory().addItem(newBook);
+			}
+			if(getConfig().getString("multi-mode").equalsIgnoreCase("true")){
+				// get a counter going
+				int count = 0;
+				while(count <= i){
+					enchID = Integer.parseInt(enchString[count].replaceAll("[\\D]",""));
+					Enchantment enchant = new EnchantmentWrapper(enchID);
+					enchLevel = item.getEnchantmentLevel(enchant);
+					EnchantmentStorageMeta multiMeta = freshMeta();
+					multiMeta.addStoredEnchant(enchant, enchLevel, true);
+					ItemStack multiBook = freshBook();
+					multiBook.setItemMeta(multiMeta);
+					count++;
+					p.getInventory().addItem(multiBook);
+				}
+				
+			}
+		}
+		
+
+	}
+	public ItemStack freshBook(){
+		ItemStack freshBook = new ItemStack(Material.ENCHANTED_BOOK);
+		return freshBook;
+	}
+	
+	public EnchantmentStorageMeta freshMeta(){
+		ItemStack book = new ItemStack(Material.ENCHANTED_BOOK);
+		EnchantmentStorageMeta freshMeta = (EnchantmentStorageMeta) book.getItemMeta();
+		return freshMeta;
+	}
+	
+	public int sacrificeToRemove(ItemStack item){
+		if(getConfig().getString("multi-mode").equalsIgnoreCase("true") == true){
+		
+			Set<Enchantment> keySet = item.getEnchantments().keySet();
+			String enchString[] = keySet.toString().split(", E");
+			return enchString.length;
+		} else return 1;
+		
 	}
 }
